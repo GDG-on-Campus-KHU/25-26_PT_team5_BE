759a0404aaa053da95ea9fe5155ee3816df3e887
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-11-22 20:00:27 +0900

    feat: Preference 엔티티 및 레포지토리 생성

--- src/main/java/com/gdg/team5/preference/domain/Preference.java
+++ src/main/java/com/gdg/team5/preference/domain/Preference.java
@@ -18,7 +18,21 @@ public class Preference {
     @Column(nullable = false, unique = true)
     private Long id;
 
+    @Column(nullable = false)
+    private String keyword;
+
+    @Column(nullable = false)
+    private String type;
+
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 }
