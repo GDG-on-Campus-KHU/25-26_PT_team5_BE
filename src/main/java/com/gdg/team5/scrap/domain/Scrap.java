d19f5c224ff108f89dfdfbbfc7643363a52c08e2
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-11-26 02:30:53 +0900

    feat: 스크랩 Entity 생성

---
+++ src/main/java/com/gdg/team5/scrap/domain/Scrap.java
@@ -0,0 +1,38 @@
+package com.gdg.team5.scrap.domain;
+
+import com.gdg.team5.auth.domain.User;
+import com.gdg.team5.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Scrap extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ScrapType type;
+
+    @Column(nullable = false)
+    private Long contentId;
+
+    @Builder
+    public Scrap(User user, ScrapType type, Long contentId) {
+        this.user = user;
+        this.type = type;
+        this.contentId = contentId;
+    }
+}
