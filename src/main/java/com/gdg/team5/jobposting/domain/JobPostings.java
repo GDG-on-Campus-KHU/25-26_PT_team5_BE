7d9bb89a1d144e8f6f0be005151262c966041bef
pyr27 <pyr8261@naver.com>
2025-11-28 20:31:56 +0900

    refactor: dto 맞춰 변경 및 리뷰반영

--- src/main/java/com/gdg/team5/jobposting/domain/JobPostings.java
+++ src/main/java/com/gdg/team5/jobposting/domain/JobPostings.java
@@ -1,15 +1,16 @@
 package com.gdg.team5.jobposting.domain;
 
+import com.gdg.team5.crawling.dto.CrawledJobsDto;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "jobs")
 @Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class JobPostings {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,14 +19,50 @@ public class JobPostings {
     private String externalId;
     private String title;
     private String companyName;
-    @Lob
-    private String content;
     private String url;
     private String postedDate;
     private String deadLine;
     private String category;
-    private String tech_stack;
+    private String techStack;
     private String location;
-    private String exp_level;
+    private String expLevel;
     private String thumbnailUrl;
+
+    @Lob
+    private String content;
+
+    @Builder
+    public JobPostings(String externalId, String title, String companyName,
+                       String url, String postedDate, String deadLine,
+                       String category, String techStack, String location,
+                       String expLevel, String thumbnailUrl, String content) {
+        this.externalId = externalId;
+        this.title = title;
+        this.companyName = companyName;
+        this.url = url;
+        this.postedDate = postedDate;
+        this.deadLine = deadLine;
+        this.category = category;
+        this.techStack = techStack;
+        this.location = location;
+        this.expLevel = expLevel;
+        this.thumbnailUrl = thumbnailUrl;
+        this.content = content;
+    }
+
+
+    public void updateFromJobsDto(CrawledJobsDto crawledJobsDto) {
+        this.externalId = crawledJobsDto.externalId();
+        this.title = crawledJobsDto.title();
+        this.companyName = crawledJobsDto.companyName();
+        this.content = crawledJobsDto.content();
+        this.url = crawledJobsDto.url();
+        this.postedDate = crawledJobsDto.postedDate();
+        this.deadLine = crawledJobsDto.deadLine();
+        this.category = crawledJobsDto.category();
+        this.techStack = crawledJobsDto.techStack();
+        this.location = crawledJobsDto.location();
+        this.expLevel = crawledJobsDto.expLevel();
+        this.thumbnailUrl = crawledJobsDto.thumbnailUrl();
+    }
 }
--- src/main/java/com/gdg/team5/jobposting/service/JobPostingService.java
+++ src/main/java/com/gdg/team5/jobposting/service/JobPostingService.java
@@ -6,38 +6,53 @@ import com.gdg.team5.jobposting.repository.JobPostingsRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class JobPostingService {
-    private final JobPostingsRepository JobPostingsRepository;
+    private final JobPostingsRepository jobPostingsRepository;
 
     // 크롤링된 채용공고 저장 (업데이트 or 신규 생성)
     public void saveCrawledJobPostings(List<CrawledJobsDto> items) {
 
+        if (items == null || items.isEmpty()) {
+            log.info("크롤링된 채용공고 없음");
+            return;
+        }
+
         for (CrawledJobsDto dto : items) {
 
             // 1. 기존 채용 공고 찾기
-            JobPostings jobPostings = JobPostingsRepository.findByExternalId(dto.getExternalId())
-                .orElse(new JobPostings());
+            JobPostings jobPostings = jobPostingsRepository
+                .findByExternalId(dto.externalId())
+                .orElse(null);
 
-            jobPostings.setExternalId(dto.getExternalId());
-            jobPostings.setTitle(dto.getTitle());
-            jobPostings.setCompanyName(dto.getCompanyName());
-            jobPostings.setContent(dto.getContent());
-            jobPostings.setUrl(dto.getUrl());
-            jobPostings.setPostedDate(dto.getPostedDate());
-            jobPostings.setDeadLine(dto.getDeadLine());
-            jobPostings.setCategory(dto.getCategory());
-            jobPostings.setTech_stack(dto.getTech_stack());
-            jobPostings.setLocation(dto.getLocation());
-            jobPostings.setExp_level(dto.getExp_level());
-            jobPostings.setThumbnailUrl(dto.getThumbnailUrl());
+            // 2. 없을 경우 신규 생성
+            if (jobPostings == null) {
+                jobPostings = JobPostings.builder()
+                    .externalId(dto.externalId())
+                    .title(dto.title())
+                    .content(dto.content())
+                    .postedDate(dto.postedDate())
+                    .deadLine(dto.deadLine())
+                    .category(dto.category())
+                    .url(dto.url())
+                    .location(dto.location())
+                    .thumbnailUrl(dto.thumbnailUrl())
+                    .companyName(dto.companyName())
+                    .techStack(dto.techStack())
+                    .expLevel(dto.expLevel())
+                    .build();
+            } else {
+                jobPostings.updateFromJobsDto(dto);
+            }
 
-            JobPostingsRepository.save(jobPostings);
+            jobPostingsRepository.save(jobPostings);
         }
 
         log.info("크롤링된 채용공고 {}건 저장 완료", items.size());
