7b363478df390ffccb94576fcf62c78136b2c333
shg4860 <shg4860@khu.ac.kr>
2025-12-02 14:12:27 +0900

    refactor: 필요 없는 클래스 대체 코멘트 반영

--- src/main/java/com/gdg/team5/auth/service/UserService.java
+++ src/main/java/com/gdg/team5/auth/service/UserService.java
@@ -3,6 +3,8 @@ package com.gdg.team5.auth.service;
 import com.gdg.team5.auth.domain.User;
 import com.gdg.team5.auth.dto.UserInfoResponse;
 import com.gdg.team5.auth.repository.UserRepository;
+import com.gdg.team5.common.exception.BaseException;
+import com.gdg.team5.common.response.BaseResponseStatus; // 추가
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +15,9 @@ public class UserService {
     private final UserRepository userRepository;
 
     public UserInfoResponse getUserInfo(String email) {
+        
         User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
+                .orElseThrow(() -> new BaseException(BaseResponseStatus.USER_NOT_FOUND));
 
         return UserInfoResponse.from(user);
     }
--- src/main/java/com/gdg/team5/common/exception/BaseException.java
+++ src/main/java/com/gdg/team5/common/exception/BaseException.java
@@ -1,13 +1,20 @@
 package com.gdg.team5.common.exception;
 
 import com.gdg.team5.common.response.BaseResponseStatus;
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
+
 public class BaseException extends RuntimeException {
     private BaseResponseStatus status;
+
+    public BaseException(BaseResponseStatus status) {
+        
+        super(status.getMessage()); 
+        
+        this.status = status;
+    }
 }
